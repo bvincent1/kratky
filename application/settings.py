c9130ba192bd554b0af7e29ce9ce2caa204f0ac7
Ben Vincent <benjc.vincent@gmail.com>
2024-11-11 21:23:32 -0600

    Init & register kits submodule

--- application/settings.py
+++ application/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "accounts",
+    "kits"
 ]
 
 AUTH_USER_MODEL = "accounts.CustomUser"
--- application/urls.py
+++ application/urls.py
@@ -18,10 +18,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("kits/", include("kits.urls")),
+    path("", RedirectView.as_view(url="kits/")),
 ]
